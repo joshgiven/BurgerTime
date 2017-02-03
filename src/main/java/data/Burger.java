51eaba269627d2482ee079c8312fb2d689fdf811
Josh Given <josh.given@gmail.com>
2017-02-03 14:00:15 -0700

    stub out java files

---
+++ src/main/java/data/Burger.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Burger {
+	private String name;
+	private List buns;
+	private List sauces;
+	private List cheeses;
+	private List proteins;
+	private List toppings;
+}
